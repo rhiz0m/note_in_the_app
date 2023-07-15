1282ba66468c99415878f91c6b2dca2312e2928b
taokyone <antonsson.andreas@hotmail.com>
2023-07-15 14:33:37 +0200

    Setup Hilt/Dagger , dependency injection

---
+++ app/src/main/java/com/taokyone/noteintheapp/di/DatabaseModule.kt
@@ -0,0 +1,33 @@
+package com.taokyone.noteintheapp.di
+
+import android.content.Context
+import androidx.room.Room
+import com.taokyone.noteintheapp.ToDoDatabase
+import com.taokyone.noteintheapp.utils.Constants.DATABASE_NAME
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+// Here we describe how we want to provide instances of certain types that we will inject later
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DatabaseModule {
+
+    @Singleton
+    @Provides
+    fun provideDatabase(
+        @ApplicationContext context: Context) = Room.databaseBuilder(
+        context,
+        ToDoDatabase::class.java,
+        DATABASE_NAME
+        ).build()
+
+    @Singleton
+    @Provides
+    fun provideDao(database: ToDoDatabase) = database.toDoDao()
+
+}
